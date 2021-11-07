08a4e4e81a237b956f74d0d1da68e9f8b60a4c39
dobrovolskyv <malak3@mail.ru>
2021-11-08 01:39:12 +0300

    done

--- js/main.js
+++ js/main.js
@@ -29,6 +29,9 @@ $(function () {
       $(".header__right-img path").removeClass("change-color");
     }
   );
+  $(".header__cart").on("click", function () {
+    $(".account-link__menu-cart").toggle("hide");
+  });
   $(".header__account").on("click", function () {
     $(".account-link__menu ").toggle("hide");
   });
@@ -44,19 +47,40 @@ $(function () {
       $(".header__cart-span").removeClass("header__cart-span--hover");
     }
   );
+  $(".nav__item").on("click", function () {
+    $(this).children("div").toggle("list--active");
+    $(this).children("ul").toggle("list--active");
+  });
 
   $(".burger").on("click", function () {
-    $(".nav__list").addClass("list--visiable");
+    if (parseInt($(window).width()) < 1200) {
+      $(".nav").slideToggle();
+    }
   });
 
   $(".nav__item").hover(
     function () {
-      $(this).children().addClass("list--active");
+      if (parseInt($(window).width()) > 1200) {
+        $(this).children("div").addClass("list--active");
+        $(this).children("ul").addClass("list--active");
+      }
     },
     function () {
-      $(this).children().removeClass("list--active");
+      if (parseInt($(window).width()) > 1200) {
+        $(this).children("ul").removeClass("list--active");
+        $(this).children("div").removeClass("list--active");
+      }
     }
   );
+
+  $(".nav__item").on("click", function (e) {
+    e.preventDefault();
+    if (parseInt($(window).width()) < 1200) {
+      $(this).children("div").addClass("list--active-visible");
+      $(this).children("ul").toggle("list--active-media");
+    }
+  });
+
   $(".item__blender-link").hover(
     function () {
       $(".blender__list").css({ display: "block" });
@@ -65,9 +89,6 @@ $(function () {
       $(".blender__list").css({ display: "none" });
     }
   );
-  $(".sponsors__slides").hover(function () {
-    $(".sponsors__arrow-left").css({ visibility: "visiable" });
-  });
 
   $(".tabheader__item").on("click", function (e) {
     e.preventDefault();
@@ -86,21 +107,30 @@ $(function () {
       $(".tabcontent__btn").css({ display: "none" });
     }
   );
-  $(".location__list-title").on("click", function () {
+  $(".sponsors__slides").hover(
+    function () {
+      $(".sponsors__arrow-left, .sponsors__arrow-right").css({ visibility: "visible" });
+    },
+    function () {
+      $(".sponsors__arrow-left, .sponsors__arrow-right").css({ visibility: "hidden" });
+    }
+  );
+
+  $(".location__list-title, .inner__info-store-arrow").on("click", function () {
     $(".location__list").toggle("list--active");
   });
 
-  $(".footer__inner-title").on("click", () => {
+  $(".footer__inner-title, .inner__info-arrow").on("click", () => {
     $(".info__list").toggle("list--active");
   });
-  $(".info-title__account").on("click", () => {
+  $(".info-title__account, .inner__info-account-arrow").on("click", () => {
     $(".footer__account__list").toggle("list--active");
   });
-  $(".info__form-title").on("click", () => {
+  $(".info__form-title, .inner__info-sign-arrow").on("click", () => {
     $(".form__input-area ").toggle("list--active");
   });
 
-  $(".social__title").on("click", () => {
-    $(".social__list").toggle("list--active");
+  $(".socials").on("click", () => {
+    $(".social__list, .accrordeon__arrow").toggle("list--active");
   });
 });
